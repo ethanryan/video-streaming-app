e996bc618d9d94f167c2e7d3703ac7ba0f0d4543
ethanryan <ethanryan@gmail.com>
2017-10-06 14:17:00 -0400

    fixed error in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,6 @@ ContentProvider.create({name: "HBO"})
 
 License.create({content_provider_id: 1, exclusive: 1, terms_in_years: 3})
 
-Video.create({content_provider_id: 1, genre_id: 1, title: "Alien", content_type: "movie", concurrent_subscriber_limit: 25})
+Video.create({license_id: 1, genre_id: 1, title: "Alien", content_type: "movie", concurrent_subscriber_limit: 25})
 
 SubscriberVideoStream.create({subscriber_id: 1, video_id: 1})
