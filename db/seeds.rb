e8fb4efbdbc3258379cca3316c21857d8c7df027
ethanryan <ethanryan@gmail.com>
2017-10-06 17:42:48 -0400

    added Faker gem to generate date for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,7 +11,7 @@
 
 Subscriber.create({first_name: "Bob", payment_type: "credit card"})
 
-CreditCard.create({subscriber_id: 1, account_number: 11111111, issuer: "Visa"})
+CreditCard.create({subscriber_id: 1, account_number: "1111111111111111", issuer: "Visa"})
 
 Account.create({subscriber_id: 1})
 
@@ -24,3 +26,19 @@ License.create({content_provider_id: 1, exclusive: 1, terms_in_years: 3})
 Video.create({license_id: 1, genre_id: 1, title: "Alien", content_type: "movie", concurrent_subscriber_limit: 25})
 
 SubscriberVideoStream.create({subscriber_id: 1, video_id: 1})
+
+50.times do
+  Subscriber.create({
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  payment_type: "credit card"
+  })
+end
+
+40.times do |index|
+  CreditCard.create({
+  subscriber_id: index,
+  account_number: Faker::Number.number(16).to_s,
+  issuer: "MasterCard"
+  })
+end
