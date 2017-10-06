e0b96e094c423cb48495405dda327d1b077adeaf
ethanryan <ethanryan@gmail.com>
2017-10-06 18:14:41 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,10 +35,60 @@ SubscriberVideoStream.create({subscriber_id: 1, video_id: 1})
   })
 end
 
-40.times do |index|
+50.times do |index|
   CreditCard.create({
   subscriber_id: index,
   account_number: Faker::Number.number(16).to_s,
-  issuer: "MasterCard"
+  issuer: ["Visa", "MasterCard"].sample
   })
+end
+
+50.times do |index|
+  Account.create({
+  subscriber_id: index
+  })
+end
+
+50.times do |index|
+  Plan.create({
+  account_id: index,
+  tier: ["Gold", "Silver", "Bronze"].sample
+  })
+end
+
+50.times do |index|
+  Genre.create({
+  name: Faker::Book.genre
+  })
+end
+
+50.times do |index|
+  ContentProvider.create({
+    name: ["Warner Bros", "Paramount", "20th Century Fox", "Disney", "Sony", "Universal", "Lions Gate Entertainment", "The Weinstein Company", "MGM", "DreamWorks"].sample
+    })
+end
+
+50.times do |index|
+  License.create({
+    content_provider_id: index,
+    exclusive: 1,
+    terms_in_years: [1, 3, 5].sample
+    })
+end
+
+500.times do |index|
+  Video.create({
+    license_id: index,
+    genre_id: rand(1..10),
+    title: Faker::Book.title,
+    content_type: ["movie", "movie", "TV show"].sample,
+    concurrent_subscriber_limit: 25
+    })
+end
+
+1000.times do |index|
+  SubscriberVideoStream.create({
+    subscriber_id: rand(1..50),
+    video_id: rand(1..500)
+    })
 end
